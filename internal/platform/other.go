1ad328f1e35b5927f6f3f762765e2c236ac3ca15
Steve Clarke <steve@sevenview.ca>
2026-03-16 00:52:11 -0230

    fix: add non-darwin platform stubs for Linux CI builds

---
+++ internal/platform/other.go
@@ -0,0 +1,18 @@
+//go:build !darwin
+
+package platform
+
+import "fmt"
+
+var errUnsupported = fmt.Errorf("outport setup is only supported on macOS")
+
+func isResolverInstalled() bool { return false }
+func isPlistInstalled() bool    { return false }
+
+func WriteResolverFile() error          { return errUnsupported }
+func RemoveResolverFile() error         { return errUnsupported }
+func WritePlist(_ string) error         { return errUnsupported }
+func RemovePlist() error                { return errUnsupported }
+func LoadAgent() error                  { return errUnsupported }
+func UnloadAgent() error                { return errUnsupported }
+func GeneratePlist(_ string) string     { return "" }
