905e8669e2f5b8149c66a59a9c374a6bd6eb87a4
Steve Clarke <steve@sevenview.ca>
2026-03-25 18:36:29 -0230

    feat: add example app for Linux end-to-end testing

---
+++ docker/example-app/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from example-app on port %s\n", port)
+	})
+
+	fmt.Printf("Listening on :%s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
